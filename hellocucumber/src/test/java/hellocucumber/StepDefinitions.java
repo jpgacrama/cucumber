27d9e40758f2d2aad8f72c471ec36436def02337
Jonas Gacrama <jonasgacrama@gmail.com>
2024-06-05 14:35:57 +0800

    main: Commit changes to step definitions
    Now I noeed to create code for each step of the scenario

--- hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -18,4 +18,21 @@ public class StepDefinitions {
     public void theScenarioPasses() {
     }
 
+    @Given("today is Sunday")
+    public void today_is_sunday() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_friday_yet() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
